c40052d1a94e3b235a5d3756d68b71fbc840e36f
bhaveshrordev <salujabhavesh24@gmail.com>
2024-04-05 13:55:33 +0530

    added rouge style theme

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,4 +1,12 @@
 module NotesHelper
+  require "redcarpet"
+  require "rouge"
+  require "rouge/plugins/redcarpet"
+
+  class RougeHTML < Redcarpet::Render::HTML
+    include Rouge::Plugins::Redcarpet
+  end
+
   def markdown(text)
     # extensions = [ :highlight ]
     extensions = %i[
@@ -10,11 +18,31 @@ module NotesHelper
   end
 
   def advanced_markdown(text)
-    render_options = { no_links: false, hard_wrap: true, link_attributes: { target: '_blank'} }
-    extensions = { highlight: true, hard_wrap: true }
+    render_options = { 
+      no_links: false, 
+      hard_wrap: true, 
+      link_attributes: { target: '_blank'} 
+    }
 
-    renderer = Redcarpet::Render::HTML.new(render_options)
-    Redcarpet::Markdown.new(renderer, extensions).render(text)
+    extensions = { 
+      highlight: true, 
+      hard_wrap: true,
+      autolink: true,
+      no_intra_emphasis: true,
+      tables: true,
+      fenced_code_blocks: true,
+      disabled_intended_code_blocks: true,
+      strikethrough: true,
+      lax_spacing: true,
+      space_after_headers: true,
+      quote: true,
+      footnotes: true,
+      underline: true
+    }
+
+    # renderer = Redcarpet::Render::HTML.new(render_options)
+    renderer = RougeHTML.new(render_options)
+    Redcarpet::Markdown.new(renderer, extensions).render(text).html_safe
   end
 
 end
